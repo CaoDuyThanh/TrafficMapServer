7805fcd0902569ad448bc391f2e3efe23a226efd
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-02 08:06:12 +0700

    Add street autocomplete for quick search

--- utils/StreetHandler.js
+++ utils/StreetHandler.js
@@ -1,3 +1,6 @@
+// LOAD LIBRARY
+var autoComplete = require('autocomplete');
+
 // LOAD CONFIG
 var config = require('../configuration.js');
 
@@ -17,6 +20,7 @@ var LoadStreets = function(){
 			return next(err);
 		}
 
+		// Create streets
 		global.AllStreets = {};
 		allStreets.forEach(function(street){
 			global.AllStreets[street.street_id] = street;
@@ -25,8 +29,38 @@ var LoadStreets = function(){
 		// Console result
 		console.log("Load street data from database successfully !");
 		console.log("Number of streets = " + Object.keys(global.AllStreets).length);
+
+		// Create street name mapping
+		global.AllStreetsName = {};
+		allStreets.forEach(function (street){
+			var streetName = street.name;
+			if (streetName != ""){
+				if (!global.AllStreetsName[streetName]){
+					global.AllStreetsName[streetName] = {};
+					global.AllStreetsName[streetName].segments = [];
+				}
+
+				global.AllStreetsName[streetName].segments.push(street.segments);
+			}			
+		});
+
+		// Console result
+		console.log("Create street name from all streets data !");
+		console.log("Number of streets has name = " + Object.keys(global.AllStreetsName).length);
+
+		// Create street dictionary for suggestion
+		global.AllStreetsDict = autoComplete.connectAutocomplete();
+		global.AllStreetsDict.initialize(function(onReady) { onReady([]); });	// Create blank dictionary
+		Object.keys(global.AllStreetsName).forEach(function(key) {
+			global.AllStreetsDict.addElement(key);
+		});
+		// Console result
+		console.log("Create street dictionary from all streets name !");
+
 		isDataLoaded = true;
 	});
 }
+
+
 
 module.exports.LoadStreets = LoadStreets;
