817b1d30276e842ce2c2d82a9e8a5b86c728307d
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-09 15:38:15 +0700

    Modified:
    - Remove _ste/_ets

--- models/SegmentHistoryModel.js
+++ models/SegmentHistoryModel.js
@@ -5,10 +5,8 @@ var schemaTypes = mongoose.Schema.Types;
 var SegmentHistorySchema = new mongoose.Schema({
   _id: schemaTypes.ObjectId,
   segment_id: schemaTypes.Long,
-  density_ste: [schemaTypes.Mixed],
-  velocity_ste: [schemaTypes.Mixed],
-  density_ets: [schemaTypes.Mixed],
-  velocity_ets: [schemaTypes.Mixed]
+  density: [schemaTypes.Mixed],
+  velocity: [schemaTypes.Mixed],
 });
 
 module.exports = mongoose.model('segment_history', SegmentHistorySchema);
