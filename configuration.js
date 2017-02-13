9d3f69749d8f20855d0a6f41cd5d7eccf0ffdaaf
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-13 09:59:24 +0700

    Modified:
    - Refactor code

--- configuration.js
+++ configuration.js
@@ -1,23 +1,18 @@
 var config = require('config');
 
-console.log('Load system configuration...')
-
 var ServerConfig;
 if (config.has('Server')){
 	ServerConfig = config.get('Server');
-	console.log('	Load server config (Done) !');
 }
 
 var DbConfig;
 if (config.has('Database')){
 	DbConfig = config.get('Database');
-	console.log('	Load database config (Done) !');
 }
 
 var MapConfig;
 if (config.has('ClientMap')){
 	MapConfig = config.get('ClientMap');
-	console.log('	Load map config (Done) !');
 }
 
 module.exports.ServerConfig = ServerConfig;
