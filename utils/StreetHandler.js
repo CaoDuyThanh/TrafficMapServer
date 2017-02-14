9b18a107883829870bbf7b5844e59f73f390afb2
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-14 17:15:57 +0700

    Modified:
    - Remove global.AllStreets

--- utils/StreetHandler.js
+++ utils/StreetHandler.js
@@ -4,35 +4,17 @@ var autoComplete = require('autocomplete');
 // LOAD CONFIG
 var config = require('../configuration.js');
 
-// LOAD SEGMENTS MODEL
-var mongoose = require('mongoose');
-var Streets = require('../models/StreetModel.js');
-
-// VARIABLES ---------------------------
-var isDataLoaded = false;
+// LOAD SERVICE
+var streetService = require('../service/street-service');
 
 // FUNCTIONS ---------------------------
 // LOAD DENSITY DATA FROM DATABASE
 var LoadStreets = function(){
-	Streets.find(function(err, allStreets){
-		if (err){ 
-			console.error("Error: Can not load street data from database ! " + err);
-			return next(err);
-		}
-
-		// Create streets
-		global.AllStreets = {};
-		allStreets.forEach(function(street){
-			global.AllStreets[street.street_id] = street;
-		});
-
-		// Console result
-		console.log("Load street data from database successfully !");
-		console.log("Number of streets = " + Object.keys(global.AllStreets).length);
-
+	var promise = new Promise((resolve, reject) => streetService.GetAllStreets(resolve, reject));
+	promise.then((data) => {
 		// Create street name mapping
 		global.AllStreetsName = {};
-		allStreets.forEach(function (street){
+		data.forEach(function (street){
 			var streetName = street.name;
 			if (streetName != ""){
 				if (!global.AllStreetsName[streetName]){
@@ -43,10 +25,10 @@ var LoadStreets = function(){
 				global.AllStreetsName[streetName].segments = global.AllStreetsName[streetName].segments.concat(street.segments);
 			}			
 		});
-
 		// Console result
 		console.log("Create street name from all streets data !");
 		console.log("Number of streets has name = " + Object.keys(global.AllStreetsName).length);
+
 
 		// Create street dictionary for suggestion
 		global.AllStreetsDict = autoComplete.connectAutocomplete();
@@ -57,7 +39,9 @@ var LoadStreets = function(){
 		// Console result
 		console.log("Create street dictionary from all streets name !");
 
-		isDataLoaded = true;
+	});
+	promise.catch((err) => {
+		console.error("Error: Can not load all streets from database ! " + err);
 	});
 }
 
