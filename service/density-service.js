1a7b8fad1f28cb576286c7da5c9c34e7f750c75b
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-15 11:42:01 +0700

    Modified:
    - Add GetDensityByStreetIds in streetService

--- service/density-service.js
+++ service/density-service.js
@@ -6,7 +6,8 @@ var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
 // Load models
-var segmentModel = require('../models/SegmentModel.js');
+var segmentModel = require('../models/SegmentModel');
+var streetModel = require('../models/StreetModel');
 
 /**
  * [GetDensityBySegmentIds - Get density based on a list of segmentId]
@@ -46,11 +47,70 @@ var GetDensityBySegmentIds = function(segmentIds, resolve, reject) {
 	});
 	// Error
 	promise.catch((err) => {
-		if (err) {
-			console.error("Error: Can not load density by segmentIds ! " + err);
-			reject(err);
-		}
+		console.error("Error: Can not load density by segmentIds ! " + err);
+		return reject(err);
+	});
+}
+
+var GetDensityByStreetIds = function(streetIds, resolve, reject) {
+	var promise = streetModel.aggregate([	{ "$match": { "street_id": { $in: streetIds } } },
+											{ "$unwind": "$segments" },
+											{ "$lookup": {
+														    "localField": "segments",
+														    "from": "segments",
+														    "foreignField": "segment_id",
+														    "as": "segmentObjects"
+														}
+											},
+											{ "$unwind": {
+															"path": "$segmentObjects",
+															"preserveNullAndEmptyArrays": true 
+														}
+											},
+											{ "$lookup": {
+															"from": "nodes",
+														    "localField": "segmentObjects.node_start",
+														    "foreignField": "node_id",
+														    "as": "segmentObjects.node_start"
+														}
+											},
+											{ "$lookup": {
+														    "from": "nodes",
+														    "localField": "segmentObjects.node_end",
+														    "foreignField": "node_id",
+														    "as": "segmentObjects.node_end"
+														}
+											},
+											{ "$project": {
+															"segmentObjects.segment_id": 1,
+														    "segmentObjects.node_start.lat": 1,
+														    "segmentObjects.node_start.lon": 1,
+														    "segmentObjects.node_end.lat": 1,
+														    "segmentObjects.node_end.lon": 1,
+														    "segmentObjects.density": 1,
+														    "segmentObjects.velocity": 1
+														  } 
+											}
+											,											
+											{ "$unwind": "$segmentObjects" },
+											{ "$group": {
+														    "_id": "$_id",
+														    "segments": { "$push": "$segmentObjects" },
+														}
+											}
+											])
+							 .exec();
+
+	// Result
+	promise.then((data) => {
+		return resolve(data);
+	});
+	// Error
+	promise.catch((err) => {
+		console.error("Error: Can not load density by streetIds ! " + err);
+		return reject(err);		
 	});
 }
 
 module.exports.GetDensityBySegmentIds = GetDensityBySegmentIds;
+module.exports.GetDensityByStreetIds = GetDensityByStreetIds;
