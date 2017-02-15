9ca9b163d8e5e464653639f1f71f9da99a3f7eb0
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-15 22:29:08 +0700

    Add:
    - Commit new files

---
+++ models/SegmentDensityModel.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+require('mongoose-long')(mongoose);
+var schemaTypes = mongoose.Schema.Types;
+
+var HistorySchema = new mongoose.Schema({
+	timestamp: Number,
+	density: Number,
+	velocity: Number
+},{ _id : false });
+
+
+var SegmentDensitySchema = new mongoose.Schema({
+	_id: schemaTypes.ObjectId,
+	segment_id: Number,
+	density: Number,
+	velocity: Number,
+	history: [HistorySchema]
+});
+
+module.exports = mongoose.model('segment_density', SegmentDensitySchema);
