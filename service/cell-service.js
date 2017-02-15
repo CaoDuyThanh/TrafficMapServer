f0ac3e15156b3d4c0e78e5496e62ef83d56772c5
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-15 22:30:07 +0700

    Modified:
    - Add service get cell by cellId attached data of segment and latlng

--- service/cell-service.js
+++ service/cell-service.js
@@ -12,25 +12,26 @@ var cellModel = require('../models/CellModel.js');
  * Get Cell based on cellId
  * @param {[type]}   cellId   [number]
  */
-var GetCell = function(cellId) {
+var GetCell = function(cellId, resolve, reject) {
 	var promise = cellModel.find({'cell_id': cellId}).exec();
 
 	// Result
 	promise.then(function(data) {
-		return data;
+		return resolve(data);
 	});
 
 	// Error
 	promise.catch(function(err) {
-		if (err) {
-			console.error("Error: Can not load cell data from database ! " + err);
-			throw err;
-		}
+		console.error("Error: Can not load cell data from database ! " + err);
+		return reject(err);
 	});
 }
 
 
-
+/**
+ * [GetCells - Get list of cell based on list of CellIds]
+ * @param {[Array[Number]]} cellIds [description]
+ */
 var GetCells = function(cellIds, resolve, reject) {
 	var promise = cellModel.find({ 'cell_id': { $in: cellIds } }).exec();
 
@@ -41,14 +42,70 @@ var GetCells = function(cellIds, resolve, reject) {
 
 	// Error
 	promise.catch(function(err) {
-		if (err) {
-			console.error("Error: Can not load cell data from database ! " + err);
-			throw err;
-		}
+		console.error("Error: Can not load cell data from database ! " + err);
+		return reject(err);
 	});
-
 }
 
 
+var GetCellSegmentsLatlng = function(cellId, resolve, reject) {
+	var promise = cellModel.aggregate([	{ "$match": { "cell_id": cellId } },
+										{ "$unwind": "$segments" },
+										{ "$lookup": {
+													    "localField": "segments",
+													    "from": "segments",
+													    "foreignField": "segment_id",
+													    "as": "segmentObjects"
+													}
+										},
+										{ "$unwind": {
+														"path": "$segmentObjects",
+														"preserveNullAndEmptyArrays": true 
+													}
+										},
+										{ "$lookup": {
+														"from": "nodes",
+													    "localField": "segmentObjects.node_start",
+													    "foreignField": "node_id",
+													    "as": "segmentObjects.node_start"
+													}
+										},
+										{ "$lookup": {
+													    "from": "nodes",
+													    "localField": "segmentObjects.node_end",
+													    "foreignField": "node_id",
+													    "as": "segmentObjects.node_end"
+													}
+										},
+										{ "$project": {
+														"segmentObjects.segment_id": 1,
+													    "segmentObjects.node_start.lat": 1,
+													    "segmentObjects.node_start.lon": 1,
+													    "segmentObjects.node_end.lat": 1,
+													    "segmentObjects.node_end.lon": 1,
+													  } 
+										},											
+										{ "$unwind": "$segmentObjects" },
+										{ "$group": {
+													    "_id": "$_id",
+													    "segments": { "$push": "$segmentObjects" },
+													}
+										}
+									])
+							 .exec();
+
+	// Result
+	promise.then(function(data) {
+		return resolve(data);
+	});
+
+	// Error
+	promise.catch(function(err) {
+		console.error("Error: Can not load cell by CellId from database ! " + err);
+		return reject(err);
+	});	
+}
+
 module.exports.GetCell = GetCell;
 module.exports.GetCells = GetCells;
+module.exports.GetCellSegmentsLatlng = GetCellSegmentsLatlng;
