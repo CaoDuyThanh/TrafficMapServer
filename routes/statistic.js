38449e0481b4eb91530321714fe7bd342d7da75b
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-17 15:56:09 +0700

    Modified:
    - Get current camera density and send back to client

--- routes/statistic.js
+++ routes/statistic.js
@@ -2,6 +2,14 @@
 var express = require('express');
 var router = express.Router();
 
+// IMPORT MAP CONFIG
+var config = require('../configuration');
+var dbConfig = config.DbConfig;
+
+// IMPORT SERVICE
+var simulationService = require('../service/simulation-service');
+
+
 router.use(function(req, res, next) {
 	if (req.method === 'OPTIONS'){
 	  	res.header("Access-Control-Allow-Origin", "*");
@@ -82,24 +90,54 @@ router.get('/vehicles/camera', function(req, res, next){
 	res.header("Access-Control-Allow-Origin", "*");
   	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
+	// Get parameters
+	var poleId = +req.query.pole_id;
+	var streamId = req.query.stream_id;
+
   	// Execute
-  	var date = new Date();
-    var UTCTime = Date.UTC(
-        date.getFullYear(),
-        date.getMonth() + 1,
-        date.getDate(),
-        date.getHours(),
-        date.getMinutes(), 
-        date.getSeconds()
-    );
-  	var responseData = {
-		status: 'success',
-		data: {
-			num_vehicles: Math.floor(Math.random() * 50),
-			utc_time: UTCTime
+  	var currentTimestamp = Date.now();
+  	var promise = new Promise((resolve, reject) => simulationService.GetCameraDensity(poleId, streamId, currentTimestamp, resolve, reject));
+  	promise.then((data) => {
+  		var date = new Date();
+	    var UTCTime = Date.UTC(
+	        date.getFullYear(),
+	        date.getMonth() + 1,
+	        date.getDate(),
+	        date.getHours(),
+	        date.getMinutes(), 
+	        date.getSeconds()
+	    );
+
+	    var cameraDensity;
+		if (data.length === 0) {
+			// New camera density
+			cameraDensity = {};
+			cameraDensity.density = 0;
+		} else {
+			cameraDensity = data[0];
 		}
-	};
-	res.json(responseData);
+
+	  	var responseData = {
+			status: 'success',
+			data: {
+				num_vehicles: cameraDensity.density,
+				utc_time: UTCTime
+			}
+		};
+		res.json(responseData);
+  	});
+  	promise.catch((err) => {
+  		var responseData = {
+			status: 'failure',
+			message: 'Can not get camera density !'
+		};
+		res.json(responseData);	
+  	})
+  	
+
+
+
+  	
 });
 
 
